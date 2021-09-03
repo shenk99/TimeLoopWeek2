538f795299fd9cf0635c505928a604739f6223d7
Owner <Owner@DESKTOP-R9C5GV3>
2021-09-03 14:19:17 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace TimeLoopWeek2
+{
+    public class timeloop
+    {
+        public static void Main(String[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            for (int x = 1; x <= num; x++)
+                Console.WriteLine(x + " Abracadabra");
+        }
+    }
+}
